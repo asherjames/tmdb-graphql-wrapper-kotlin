8c52d495c9385a4c6d4552a898232b1c77a10610
asherjames <asherjames.nz@gmail.com>
2018-01-16 19:20:30 +1300

    Add Fuel depedency, create first TmdbUtil method

--- src/main/kotlin/ash/kotlin/graphql/data/TmdbUtil.kt
+++ src/main/kotlin/ash/kotlin/graphql/data/TmdbUtil.kt
@@ -1,13 +1,32 @@
 package ash.kotlin.graphql.data
 
 import ash.kotlin.graphql.AppConfig
+import ash.kotlin.graphql.TmdbGqlException
 import ash.kotlin.graphql.types.genre.GenreType
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.gson.responseObject
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import javax.inject.Inject
 
-class TmdbUtil(private val config: AppConfig)
+class TmdbUtil @Inject constructor(config: AppConfig)
 {
+    init
+    {
+        FuelManager.instance.basePath = config.baseUrl
+    }
 
     fun getGenreList(): List<GenreType>
     {
+        var data: List<GenreType> = emptyList()
 
+        "/genre/movie/list".httpGet().responseObject<List<GenreType>> {_, _, result ->
+            when (result) {
+                is Result.Failure -> throw TmdbGqlException(result.error)
+                is Result.Success -> data = result.get()
+            }
+        }
+
+        return data
     }
 }
