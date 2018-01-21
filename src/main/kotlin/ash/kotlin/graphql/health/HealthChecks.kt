6eb847c570aa2cf2f047b06d6ebf54d673012574
asherjames <asherjames.nz@gmail.com>
2018-01-21 19:25:10 +1300

    Fix TmdbHealthCheck bug

--- src/main/kotlin/ash/kotlin/graphql/health/HealthChecks.kt
+++ src/main/kotlin/ash/kotlin/graphql/health/HealthChecks.kt
@@ -3,9 +3,10 @@ package ash.kotlin.graphql.health
 import ash.kotlin.graphql.AppConfig
 import com.codahale.metrics.health.HealthCheck
 import com.mashape.unirest.http.Unirest
-import javax.inject.Inject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-class AppHealthCheck() : HealthCheck()
+class AppHealthCheck : HealthCheck()
 {
     override fun check(): Result
     {
@@ -15,10 +16,15 @@ class AppHealthCheck() : HealthCheck()
 
 class TmdbHealthCheck(private val config: AppConfig) : HealthCheck()
 {
+    private val log: Logger = LoggerFactory.getLogger(TmdbHealthCheck::class.java)
+
     override fun check(): Result
     {
+        log.info("TmdbHealthCheck using config: $config")
+
         val response = Unirest
                 .get(config.baseUrl)
+                .queryString(mapOf("api_key" to config.apikey))
                 .asString()
 
         return if (response.status == 200)
