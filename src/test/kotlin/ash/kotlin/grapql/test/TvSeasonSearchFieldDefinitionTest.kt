5988c08cec54de0e0ed81036489b0d5b32ba796c
asherjames <asherjames.nz@gmail.com>
2018-01-30 21:11:11 +1300

    Convert final unit test to TmdbUtil

--- src/test/kotlin/ash/kotlin/grapql/test/TvSeasonSearchFieldDefinitionTest.kt
+++ src/test/kotlin/ash/kotlin/grapql/test/TvSeasonSearchFieldDefinitionTest.kt
@@ -1,8 +1,10 @@
 package ash.kotlin.grapql.test
 
 import ash.kotlin.graphql.TmdbSchema
+import ash.kotlin.graphql.data.TmdbUtil
 import ash.kotlin.graphql.data.TvDao
 import ash.kotlin.graphql.fields.FieldDefiner
+import ash.kotlin.graphql.fields.MovieKeywordFieldDefinition
 import ash.kotlin.graphql.fields.TvSeasonSearchFieldDefinition
 import ash.kotlin.graphql.types.tvseason.TvEpisodeType
 import ash.kotlin.graphql.types.tvseason.TvGuestStarType
@@ -13,15 +15,57 @@ import com.google.gson.JsonPrimitive
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 import org.assertj.core.api.Assertions.*
-import org.junit.BeforeClass
-import org.junit.Ignore
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.any
+import org.mockito.junit.MockitoJUnitRunner
 
-@Ignore
+@RunWith(MockitoJUnitRunner.Silent::class)
 class TvSeasonSearchFieldDefinitionTest
 {
 
   private val gson: Gson = Gson()
+
+  private lateinit var seasonIdJson: JsonObject
+  private lateinit var episodeNameJson: JsonObject
+  private lateinit var crewJobJson: JsonObject
+  private lateinit var guestNameCrewIdJson: JsonObject
+
+  @Mock
+  private lateinit var tmdbutil: TmdbUtil
+
+  @InjectMocks
+  private lateinit var fieldDefinition: TvSeasonSearchFieldDefinition
+
+  @Before
+  fun setupResults()
+  {
+    setupMocks()
+
+    val schema = TmdbSchema(listOf(fieldDefinition))
+
+    val seasonIdQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){id}}")
+    seasonIdJson = extractData(seasonIdQueryResultObject)
+
+    val episodeNameQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{name}}}")
+    episodeNameJson = extractData(episodeNameQueryResultObject)
+
+    val crewJobQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{crew{job}}}}")
+    crewJobJson = extractData(crewJobQueryResultObject)
+
+    val guestNameAndCrewIdQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{crew{id} guestStars{name}}}}")
+    guestNameCrewIdJson = extractData(guestNameAndCrewIdQueryResultObject)
+
+  }
+
+  private fun setupMocks()
+  {
+    `when`(tmdbutil.getTvSeason(any(Int::class.java), any(Int::class.java))).thenReturn(getTvSeason())
+  }
 
   @Test
   fun seasonIdIsCorrect()
@@ -83,48 +127,6 @@ class TvSeasonSearchFieldDefinitionTest
   fun getResult(jsonObject: JsonObject): JsonObject
   {
     return jsonObject["tvSeasonSearch"].asJsonObject
-  }
-
-  companion object
-  {
-    private lateinit var seasonIdJson: JsonObject
-    private lateinit var episodeNameJson: JsonObject
-    private lateinit var crewJobJson: JsonObject
-    private lateinit var guestNameCrewIdJson: JsonObject
-
-    @BeforeClass
-    @JvmStatic
-    fun setupResults()
-    {
-      val schema = TmdbSchema(mockFields())
-
-      val seasonIdQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){id}}")
-      seasonIdJson = extractData(seasonIdQueryResultObject)
-
-      val episodeNameQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{name}}}")
-      episodeNameJson = extractData(episodeNameQueryResultObject)
-
-      val crewJobQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{crew{job}}}}")
-      crewJobJson = extractData(crewJobQueryResultObject)
-
-      val guestNameAndCrewIdQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{crew{id} guestStars{name}}}}")
-      guestNameCrewIdJson = extractData(guestNameAndCrewIdQueryResultObject)
-
-    }
-
-    private fun mockFields(): List<FieldDefiner>
-    {
-//      class TvDaoStub : TvDao
-//      {
-//        override fun getTvSeason(tvShowId: Int, seasonNumber: Int): TvSeasonType
-//        {
-//          return getTvSeason()
-//        }
-//      }
-//
-//      return listOf(TvSeasonSearchFieldDefinition(TvDaoStub()))
-      return emptyList()
-    }
   }
 
   private class TvEpisodeType_TestClass(@SerializedName("guestStars") var guestStars_Test: List<TvGuestStarType>) : TvEpisodeType()
